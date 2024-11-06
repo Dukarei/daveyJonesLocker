19d0f20b1e1803cb845a00c883b070fa53fa43f3
Dukarei <da00029@mix.wvu.edu>
2024-11-05 21:20:26 -0500

    Add files via upload
    added variables to be passed to backend for login/registration

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 const container = document.querySelector('.container');
 const registerBtn = document.querySelector('.register-btn');
 const loginBtn = document.querySelector('.login-btn');
-const form = document.querySelector('form');
+
+const loginForm = document.getElementById('login_form');
+const registerForm = document.getElementById('register_form');
+
+//TODO: check these against the database w/ pseudo code below, add separate checks fdr registration using the commented vars above
+ //if username already in DB, deny access
+  //if username in DB does not match password, deny access
+  //if user.hasEmail && gets 5 false attempts, lock account and send email to user
+
+
+function handleLoginSubmit(event) {
+    event.preventDefault(); 
+  
+    const username = document.getElementById('user').value;
+    const password = document.getElementById('pass').value;
+  
+    console.log('Login info:', { username, password });
+    //login stuff here
+  }
+  
+  function handleRegisterSubmit(event) {
+    event.preventDefault(); // no default form submission
+  
+    const username = document.getElementById('user_register').value;
+    const email = document.getElementById('email_register').value;
+    const password = document.getElementById('pass_register').value;
+  
+    console.log('Register info:', { username, email, password });
+    //registration stuff here
+  }
+
+  loginForm.addEventListener('submit', handleLoginSubmit);
+  registerForm.addEventListener('submit', handleRegisterSubmit);
 
 registerBtn.addEventListener('click', () => {
     container.classList.add('active');
@@ -9,36 +41,8 @@ registerBtn.addEventListener('click', () => {
 
 loginBtn.addEventListener('click', () => {
     container.classList.remove('active');
+    
 });
 
 
 
-//stuff below is from registration form - will need to get the values after submission of that form/button, prob not the one below
-/*
-const email = document.getElementById("email").value;
-const attempted_user = document.getElementById("user_register").value;
-const attempted_pass = document.getElementById("pass_register").value;
-*/
-
-
-//TODO: check these against the database w/ pseudo code below, add separate checks fdr registration using the commented vars above
-form.addEventListener('submit', (e) => {
-    //stuff here is from the login form
-    const username = document.getElementById("user").value;
-    const passwd = document.getElementById("pass").value;
-    let isValid = true;
-  //if username already in DB, deny access
-  //if username in DB does not match password, deny access
-  //if user.hasEmail && gets 5 false attempts, lock account and send email to user
-   
-  
-           
-      
-            alert(username + " " + passwd);
-      
-
-    if (!isValid) {
-        e.preventDefault();
-        alert('Invalid user/pass.');
-    }
-});
